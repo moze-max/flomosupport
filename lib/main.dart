acb8f07f06a87d1afa3d136e1214c9fe2612a5ce
moze <3023713469@qq.com>
2025-05-26 22:50:53 +0800

    基础的i18n

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flomosupport/components/newguide.dart';
 import 'package:flomosupport/pages/about.dart';
 import 'package:flomosupport/pages/homepage.dart';
 import 'package:flomosupport/pages/notification.dart' as ntfic;
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'pages/article.dart';
 import 'pages/guide.dart';
@@ -23,7 +25,21 @@ class MyApp extends StatelessWidget {
         '/about': (context) => About(),
         '/notification': (context) => ntfic.Notification(),
       },
-      title: 'Flomo Support',
+      // title: 'Flomo Support',
+      title: AppLocalizations.of(context)?.appTitle,
+      // localizationsDelegates: [
+      //   // 本地化的代理类
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: [
+      //   const Locale('en', 'US'), // 美国英语
+      //   const Locale('zh', 'CN'), // 中文简体
+      //   //其他Locales
+      // ],
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         fontFamily: 'NotoSansSC',
