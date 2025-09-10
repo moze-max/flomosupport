7a1254b0857be8a562a5b9e1d7714206e5f91728
moze <moze0021@outlook.com>
2025-09-10 17:25:14 +0800

    refactor(pages): 优化昵称设置页面逻辑
    - 在 initState 中增加了对当前昵称的检查逻辑
    - 如果昵称未设置或为空，不向 TextField 设置默认值

--- lib/pages/article/Nickname_Setting_Page.dart
+++ lib/pages/article/Nickname_Setting_Page.dart
@@ -1,3 +1,84 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:flomosupport/functions/nickname_notifier.dart';
+
+// class NicknameSettingPage extends StatefulWidget {
+//   const NicknameSettingPage({super.key});
+
+//   @override
+//   State<NicknameSettingPage> createState() => _NicknameSettingPageState();
+// }
+
+// class _NicknameSettingPageState extends State<NicknameSettingPage> {
+//   late final TextEditingController _nicknameController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     // 使用 Provider 获取当前的昵称
+//     final nicknameNotifier =
+//         Provider.of<NicknameNotifier>(context, listen: false);
+//     _nicknameController =
+//         TextEditingController(text: nicknameNotifier.currentNickname);
+//   }
+
+//   @override
+//   void dispose() {
+//     _nicknameController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('更改名字'),
+//         actions: [
+//           TextButton(
+//             onPressed: () async {
+//               // 获取 notifier 实例并更新昵称
+//               final nicknameNotifier =
+//                   Provider.of<NicknameNotifier>(context, listen: false);
+//               await nicknameNotifier.updateNickname(_nicknameController.text);
+//               // 保存后返回上一页
+//               if (mounted) {
+//                 if (!context.mounted) {
+//                   return;
+//                 }
+//                 Navigator.of(context).pop();
+//               }
+//             },
+//             child: const Text('保存', style: TextStyle(color: Colors.blue)),
+//           ),
+//         ],
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             // 昵称输入框
+//             TextField(
+//               controller: _nicknameController,
+//               decoration: const InputDecoration(
+//                 hintText: '输入新昵称',
+//                 border: UnderlineInputBorder(),
+//               ),
+//             ),
+//             const SizedBox(height: 8),
+//             // 提示文本
+//             const Text(
+//               '好名字可以让你的朋友更容易记住你。',
+//               style: TextStyle(fontSize: 12, color: Colors.grey),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// lib/pages/Nickname_Setting_Page.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flomosupport/functions/nickname_notifier.dart';
@@ -15,11 +96,20 @@ class _NicknameSettingPageState extends State<NicknameSettingPage> {
   @override
   void initState() {
     super.initState();
-    // 使用 Provider 获取当前的昵称
+    // 确保在 initState 中安全地获取 Provider 数据
     final nicknameNotifier =
         Provider.of<NicknameNotifier>(context, listen: false);
-    _nicknameController =
-        TextEditingController(text: nicknameNotifier.currentNickname);
+
+    // 检查昵称是否已加载并且不为空
+    if (nicknameNotifier.currentNickname != null &&
+        nicknameNotifier.currentNickname!.isNotEmpty &&
+        nicknameNotifier.currentNickname! != '未设置昵称') {
+      _nicknameController =
+          TextEditingController(text: nicknameNotifier.currentNickname);
+    } else {
+      // 如果没有昵称或昵称是默认值，则不设置任何文本，以显示 hintText
+      _nicknameController = TextEditingController();
+    }
   }
 
   @override
